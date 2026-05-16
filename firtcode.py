a0cf96b0b5d565c16949647de42a5d24ab715dd0
Ralston <mellewinnet45@gmail.com>
2026-05-16 19:54:46 -0400

    Updated Python lab files

--- firtcode.py
+++ firtcode.py
@@ -1,2 +1,8 @@
 message = "Hello world!"
 print(message)
+
+first_name="Sophie"
+last_name = "Lanister"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
--- lab2.py
+++ lab2.py
@@ -4,27 +4,27 @@
 
 messy_menu = "    PizZA, burGER, SaLAd "
 
-#a used .strip to remove the white space.
+#a Used .strip to remove the white space.
 removed_menu = messy_menu.strip()
 print(removed_menu)
 
-#b using .lower to maake sure that the whole string is lowercase
+#b Used .lower to make sure that the whole string is lowercase
 lower_case = removed_menu.lower()
 print(lower_case)
 
-#c outputted the message along with the menu list.
+#c Outputted the message along with the menu list.
 
 print(f"Today’s special menu list: {lower_case}" )
 
 # Question 2 
 
-#a  used list() and range() to create a list of even number from 2-50.
+#a  Used list() and range() to create a list of even number from 2-50.
 even_numList = list(range(2, 51, 2))
 
-#b used print() ro display the numbers.
+#b Used print() to display the numbers.
 print(even_numList)
 
-#c used len() and print() to calculate and print the total number of items in the list.
+#c Used len() and print() to calculate and print the total number of items in the list.
 total_items = len(even_numList)
 print(total_items)
 
@@ -32,30 +32,30 @@ print(total_items)
 num_sum = sum(even_numList)
 print(num_sum)
 
-#e
+#e Used a constant multiplier.
 MULTIPLIER = 5
 
 max_num = max(even_numList)
 min_num = min(even_numList)
 
 calculation = MULTIPLIER * (max_num + min_num)
-print(f"The produtc is: {calculation}")
+print(f"The product is: {calculation}")
 
 # Question
 
-#a  used [] to creat a list wiht four names.
+#a  Used [] to creat a list with four names.
 guest_list = ['Martin', 'David', 'Shanice', 'Riley']
 print(guest_list)
 
-#b used .append to add the name Linus to the end of the list.
+#b Used .append to add the name Linus to the end of the list.
 guest_list.append("Linus")
 print(guest_list)
 
-#c used .insert(0) to insert Guido at the beginning of the list.
+#c Used .insert(0) to insert Guido at the beginning of the list.
 guest_list.insert(0, "Guido")
 print(guest_list)
 
-#d used sort() to to sort the list alphabetically and permanently
+#d Used sort() to to sort the list alphabetically and permanently
 guest_list.sort()
 print(guest_list)
 
@@ -63,7 +63,7 @@ print(guest_list)
 invitations = [f"You are invited, {i.capitalize()}!" for i in guest_list]
 print(invitations)
 
-#f used [:3] to print only the first three invitatioins.
+#f Used [:3] to print only the first three invitatioins.
 print(invitations[:3])
 
 
